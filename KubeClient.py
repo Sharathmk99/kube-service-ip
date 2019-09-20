81c4fe1d823bf7409dec884b805c07e8cfd74899
Sharath Mandya <sharath.mandya-krishna@partners.basf.com>
2019-09-20 11:39:25 +0200

    Fixed In cluster authentication

--- KubeClient.py
+++ KubeClient.py
@@ -7,7 +7,9 @@ class KubeClient:
     def __init__(self, google_app_credentials=None):
         if google_app_credentials:
             os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = google_app_credentials
-        config.load_kube_config()
+            config.load_kube_config()
+        else:
+            config.load_incluster_config()
 
     @classmethod
     def get_kube_client(self):
